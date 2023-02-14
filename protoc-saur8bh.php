4558b2b805f710f612c250769eb71d8e0b4d96b2
Saurabh Thakur <81372210+saur8bh@users.noreply.github.com>
2023-02-14 17:06:07 +0530

    Update protoc-saur8bh.php

--- protoc-saur8bh.php
+++ protoc-saur8bh.php
@@ -1,3 +1,4 @@
+<?php
 /**
  * Plugin Name: ProToc By Saur8bh
  * Plugin URI: https://github.com/saur8bh/ProToc-saur8bh
@@ -6,55 +7,62 @@
  * Author: Saur8bh
  * Author URI: https://www.mrskt.com/
  */
-add_filter( 'the_content', 'insert_toc_before_first_h2' );
-function insert_toc_before_first_h2( $content ) {
-  // Check if the post has at least two H2 headings
-  if ( preg_match_all( '/<h2/', $content, $matches ) >= 2 ) {
-    $content = add_id_to_headings( $content );
-    $toc = generate_toc();
-    $content = preg_replace( '/<h2/', $toc . '<h2', $content, 1 );
-  }
 
-  return $content;
+function saur8bh_add_table_of_contents( $content ) {
+    if ( is_singular( array( 'post', 'page' ) ) ) {
+        $table_of_contents = '<div id="table-of-contents"><p><strong>Table of Contents</strong></p><ul>';
+        $count = 0;
+
+        // Add ID's to the headings
+        $content = preg_replace_callback( '/<h([2-6]).*>(.*)<\/h[2-6]>/i', function( $matches ) use ( &$count ) {
+            $count++;
+            return '<h' . $matches[1] . ' id="heading-' . $count . '">' . $matches[2] . '</h' . $matches[1] . '>';
+        }, $content );
+
+        // Generate table of contents
+        $dom = new DOMDocument();
+        libxml_use_internal_errors( true );
+        $dom->loadHTML( mb_convert_encoding( $content, 'HTML-ENTITIES', 'UTF-8' ) );
+        $xpath = new DOMXPath( $dom );
+
+        $headings = $xpath->query( '//h2|//h3|//h4|//h5|//h6' );
+
+        $current_level = 2;
+        $previous_level = 2;
+        $toc_counter = 0;
+        $stack = array();
+
+        foreach ( $headings as $heading ) {
+            $toc_counter++;
+
+            // Determine heading level
+            $current_level = (int) $heading->tagName[1];
+
+            // Generate anchor
+            $anchor = '<a href="#' . $heading->getAttribute( 'id' ) . '">' . $heading->nodeValue . '</a>';
+
+            // Add to table of contents
+            if ( $current_level > $previous_level ) {
+                $table_of_contents .= '<ul>';
+                array_push( $stack, '</ul>' );
+            } elseif ( $current_level < $previous_level ) {
+                $table_of_contents .= str_repeat( array_pop( $stack ), $previous_level - $current_level ) . '</li>';
+            } else {
+                $table_of_contents .= '</li>';
+            }
+
+            $table_of_contents .= '<li>' . $anchor;
+
+            $previous_level = $current_level;
+        }
+
+        $table_of_contents .= str_repeat( array_pop( $stack ), $previous_level - 2 ) . '</li></ul></div>';
+
+        // Add table of contents to content
+        $content = $table_of_contents . $content;
+    }
+
+    return $content;
 }
 
-function generate_toc() {
-  // Initialize the table of contents
-  $toc = '<div id="table-of-contents">';
-  $toc .= '<p><strong>Table of Contents</strong></p>';
-  $toc .= '<ul>';
-
-  // Get the headings in the post
-  global $post;
-  preg_match_all( '/<h([2-6]).*?>(.*?)<\/h\1>/', $post->post_content, $headings );
-
-  // Counter for the number of headings
-  $count = 0;
-
-  // Loop through the headings and add them to the table of contents
-  foreach ( $headings[0] as $heading ) {
-    $count++;
-    $slug = '#heading-' . $count;
-    $heading = preg_replace( '/<h2.*>(.*)<\/h2>/', '$1', $heading );
-    $toc .= sprintf( '<li><a href="%s">%s</a></li>', $slug, $heading );
-  }
-
-  // Close the table of contents
-  $toc .= '</ul>';
-  $toc .= '</div>';
-
-  return $toc;
-}
-
-function add_id_to_headings( $content ) {
-  // Counter for the number of headings
-  $count = 0;
-
-  // Add ID's to the headings
-  $content = preg_replace_callback( '/<h2.*>(.*)<\/h2>/', function( $matches ) use ( &$count ) {
-    $count++;
-    return '<h2 id="heading-' . $count . '">' . $matches[1] . '</h2>';
-  }, $content );
-
-  return $content;
-}
+add_filter( 'the_content', 'saur8bh_add_table_of_contents' );
